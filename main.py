874d8f7615f4a630e9cb54da71f82d0a1ce526e5
saydullayevna-07 <rejabboyevamadina6@gmail.com>
2023-11-30 11:28:53 +0500

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print(hello world)
+# 1-vazifa
+besh =int(input("juft sonni kiriting: "))
+
+if besh % 2==0:
+    print("to'g'ri")
+else:
+    print("bu juft son emas")
+
+
+
+    # 2-vazifa
+  besh =int(input("yoshingizni kiriting: "))
+if besh<5 or besh>59
+    print("chiptalar siz uchun tekin")
+else:
+    print("chiptaga to'lashingiz kerak")
+
+
+    x =int(input("yoshingizni kiriting: "))
+    if x<17
+        print("chipta narxi 10 000")
+    else:
+        print("18 yoshdan kattasan")
+
+
+        y =int(input("yoshingizni kiriting: "))
+        if y>18
+            print("chipta narxi 20 000")
+        else:
+            print("20 yoshdan kattasan")
+
+
+
+
+# 3-vazifa
+n = int(input("sonni kiriting: "))
+keyingi_son=n+1
+print(f"{n} dan keyin {keyingi_son} keladi")
+
+
+
